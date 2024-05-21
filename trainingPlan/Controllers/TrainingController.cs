a01ecf16cb9e95194d1773cf589692f5a2043fa6
Zuzia <zuzia.jarl@gmail.com>
2024-05-21 20:05:04 +0200

    training fixed

--- trainingPlan/Controllers/TrainingController.cs
+++ trainingPlan/Controllers/TrainingController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using trainingPlan.Models;
 
 namespace trainingPlan.Controllers
 {
@@ -19,21 +20,37 @@ namespace trainingPlan.Controllers
             _context = context;
         }
 
+       [HttpGet("")]
         public async Task<IActionResult> Index()
         {
             return _context.Trainings != null ?
-            View(await _context.Trainings.ToListAsync()):
+            View(await _context.Trainings.ToListAsync()) :
             Problem("Entity set is null.");
         }
 
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(Training training)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(training);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(training);
+        }
+
+        [HttpGet("error")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View("Error!");
-        }
-
-        public IActionResult Create(){
-            return View();
         }
     }
 }
