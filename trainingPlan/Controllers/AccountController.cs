e3c7bb590467ea8e34e914b685de3cb75820ab9a
M-janowska <madziajanowska7@gmail.com>
2024-05-22 16:48:05 +0200

    login/logout

--- trainingPlan/Controllers/AccountController.cs
+++ trainingPlan/Controllers/AccountController.cs
@@ -24,13 +24,46 @@ public class AccountController : Controller
     [HttpPost]
     public IActionResult Login(string username, string password)
     {
-        var user = _context.Users.SingleOrDefault(u => u.Username == username);
-        if (user != null && VerifyPasswordHash(password, user.PasswordHash))
+        // var user = _context.Users.SingleOrDefault(u => u.Username == username);
+        // if (user != null && VerifyPasswordHash(password, user.PasswordHash))
+        // {
+        //     HttpContext.Session.SetString("UserId", user.Id.ToString());
+        //     return RedirectToAction("Index", "Home");
+        // }
+        if (username == "admin" && password == "admin")
         {
-            HttpContext.Session.SetString("UserId", user.Id.ToString());
+            //HttpContext.Session.SetString("Username", username);
+            HttpContext.Session.SetString("IsAdmin", "true");
+            HttpContext.Session.SetString("LoggedIn", "true");
             return RedirectToAction("Index", "Home");
         }
         return View();
+    }
+
+    public IActionResult Register()
+    {
+        if (!UserIsAdmin())
+        {
+            return Unauthorized();
+        }
+
+        return View();
+    }
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Login");
+    }
+
+    private bool UserIsAdmin()
+    {
+        var userId = HttpContext.Session.GetString("UserId");
+        if (userId != null)
+        {
+            var user = _context.Users.Find(int.Parse(userId));
+            return user != null && user.Username == "admin";
+        }
+        return false;
     }
 
     private bool VerifyPasswordHash(string password, string storedHash)
